3eaa15072be704f8c5ff3bacdc556390236f5092
Rajminster <rajminster@gmail.com>
2017-07-10 21:38:05 -0400

    A start, dealing with a rare form of cancer known as ConstraintLayout

--- app/src/main/java/com/rnd/helius/MainActivity.java
+++ app/src/main/java/com/rnd/helius/MainActivity.java
@@ -1,13 +1,26 @@
 package com.rnd.helius;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mapButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapButton = (Button) findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
