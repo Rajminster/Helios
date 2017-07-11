f0e50e5bcd80a2873123e9829b0d5f91c7eb4fca
Rajminster <rajminster@gmail.com>
2017-07-10 22:11:13 -0400

    101 reasons why MapActivity committed Sudoku

--- app/src/main/java/com/rnd/helius/MainActivity.java
+++ app/src/main/java/com/rnd/helius/MainActivity.java
@@ -5,32 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mapButton;
     Button findButton;
+    Button ldr0Button;
+    Button ldr1Button;
+    Button ldr2Button;
+    Button ldr3Button;
+    Button powerButton;
+    ProgressBar power;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ldr0Button = (Button) findViewById(R.id.ldr0);
+        ldr1Button = (Button) findViewById(R.id.ldr1);
+        ldr2Button = (Button) findViewById(R.id.ldr2);
+        ldr3Button = (Button) findViewById(R.id.ldr3);
+
+        power = (ProgressBar) findViewById(R.id.powerBar);
+        power.setProgress(50);
+
+        powerButton = (Button) findViewById(R.id.powerButton);
+        powerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
         findButton = (Button) findViewById(R.id.findButton);
         findButton.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),BluetoothActivity.class);
+                Intent intent = new Intent(getApplicationContext(), BluetoothActivity.class);
                 startActivity(intent);
             }
         });
 
-        mapButton = (Button) findViewById(R.id.mapButton);
-        mapButton.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MapActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
