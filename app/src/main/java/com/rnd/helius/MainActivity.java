4c1c64385633a3a2be58b9181efdbd5a6c16633d
Rajminster <rajminster@gmail.com>
2017-07-10 21:53:15 -0400

    Added more adjustements to layout

--- app/src/main/java/com/rnd/helius/MainActivity.java
+++ app/src/main/java/com/rnd/helius/MainActivity.java
@@ -9,10 +9,20 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button mapButton;
+    Button findButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findButton = (Button) findViewById(R.id.findButton);
+        findButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),BluetoothActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mapButton = (Button) findViewById(R.id.mapButton);
         mapButton.setOnClickListener(new View.OnClickListener() {
