8e46068f30cad686de0726de8afade090b702856
Rajminster <rajminster@gmail.com>
2017-07-12 23:40:37 -0400

    Trying to get data from Arduino over bluetooth

--- app/src/main/java/com/rnd/helius/MainActivity.java
+++ app/src/main/java/com/rnd/helius/MainActivity.java
@@ -2,17 +2,24 @@ package com.rnd.helius;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
     Button helpButton;
     ProgressBar powerBar;
     TextView powerText;
+    Handler bluetoothIn;
+    static String address;
+    ConnectedThread mConnectedThread;
+
+    final int handlerState = 0;                         //used to identify handler message
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder recDataString = new StringBuilder();
+    BluetoothDevice tracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +48,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ldr0Button = (Button) findViewById(R.id.ldr0);
-        ldr0Button.setText((int)(Math.random() * 1000) + "");
+        ldr0Button.setText((int) (Math.random() * 1000) + "");
         ldr1Button = (Button) findViewById(R.id.ldr1);
-        ldr1Button.setText((int)(Math.random() * 1000) + "");
+        ldr1Button.setText((int) (Math.random() * 1000) + "");
         ldr2Button = (Button) findViewById(R.id.ldr2);
-        ldr2Button.setText((int)(Math.random() * 1000) + "");
+        ldr2Button.setText((int) (Math.random() * 1000) + "");
         ldr3Button = (Button) findViewById(R.id.ldr3);
-        ldr3Button.setText((int)(Math.random() * 1000) + "");
+        ldr3Button.setText((int) (Math.random() * 1000) + "");
 
         powerBar = (ProgressBar) findViewById(R.id.powerBar);
-        powerBar.setProgress((int)(Math.random() * 100));
+        powerBar.setProgress((int) (Math.random() * 100));
 
         powerText = (TextView) findViewById(R.id.powerText);
 
@@ -68,15 +84,131 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
-            if (bluetooth != null &&!bluetooth.isEnabled()) {
-                Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(turnOn, 0);
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {                 //if message is what we want
+                String readMessage = (String) msg.obj;                                                                // msg.arg1 = bytes from connect thread
+                Toast.makeText(getApplicationContext(), readMessage, Toast.LENGTH_SHORT).show();
+                Log.d("FOUND",readMessage);
             }
-            Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();
-            ArrayList list = new ArrayList();
-            for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
-            Toast.makeText(getApplicationContext(), list.toString() ,Toast.LENGTH_SHORT).show();
+        };
 
+        BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (bluetooth != null && !bluetooth.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
         }
+        Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+        for (BluetoothDevice bt : pairedDevices) {
+            if (bt.getName().equals("Solar Tracker")) {
+                tracker = bt;
+                Log.d("FOUND","FOUND");
+            }
+            list.add(bt.getName());
+        }
+        //Toast.makeText(getApplicationContext(), list.toString() ,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            //Don't leave Bluetooth sockets open when leaving activity
+            btSocket.close();
+        } catch (IOException e2) {
+            //insert code to deal with this
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Get MAC address from DeviceListActivity via intent
+        Intent intent = getIntent();
+
+        //Get the MAC address from the DeviceListActivty via EXTRA
+        address = tracker.getAddress();
+
+        //create device and set the MAC address
+        //BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = tracker.createRfcommSocketToServiceRecord(UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"));
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_LONG).show();
+        }
+        // Establish the Bluetooth socket connection.
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                //insert code to deal with this
+            }
+        }
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+        mConnectedThread.run();
+
+        //I send a character when resuming.beginning transmission to check device is connected
+        //If it is not an exception will be thrown in the write method and finish() will be called
+        //mConnectedThread.write("x");
+    }
+
+    //create new class for connect thread
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            // Keep looping to listen for received messages
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);            //read bytes from input buffer
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Send the obtained bytes to the UI Activity via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                    Log.d("LOOP",readMessage);
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
+                finish();
+
+            }
+        }
+    }
 }
