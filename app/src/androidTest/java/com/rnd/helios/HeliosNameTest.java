5fe1c55555c43d9f218c89d09b4b892fb610c56f
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-07-27 21:33:01 -0400

    Renamed file and Helios

--- app/src/androidTest/java/com/rnd/helios/HeliosNameTest.java
+++ app/src/androidTest/java/com/rnd/helios/HeliosNameTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.rnd.helius", appContext.getPackageName());
+        assertEquals("com.rnd.helios", appContext.getPackageName());
     }
 }
