298d760a5fb7d73f60c5956a3cf2fc1fdd37e0bf
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-07-28 00:35:07 -0400

    Updated HelpActivity.java

--- app/src/main/java/com/rnd/helios/HelpActivity.java
+++ app/src/main/java/com/rnd/helios/HelpActivity.java
@@ -1,8 +1,13 @@
-package com.rnd.helius;
+package com.rnd.helios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Activity which displays the help screen on the phone's display.
+ *
+ * @author Rishi Raj
+ */
 public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
